3c7658597076543481b4252426cc1e261cbf0f93
Pratap <majgepratap1504@gmail.com>
2025-02-21 09:38:50 +0530

    Create python_1.py

---
+++ python_1.py
@@ -0,0 +1,13 @@
+x = int(input("enter the number"))
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+x = int(input("Enter Second number"))
+
+if x % 2 == 0:
+
+  print("Even")
+else:
+    print("Odd")
